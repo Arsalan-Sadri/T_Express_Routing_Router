ebb62594e4f549f4109e250615bc6930a334cd42
Arsalan Sadri <asadri62@gmail.com>
2018-10-13 15:41:54 -0700

    renaming

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ const app = express();
 app.use(router);
 
 const PORT = process.env.PORT || 8080;
-const host = "localhost"
+const host = "localhost";
 app.listen(PORT, host, () => console.log("App running on http://" + host + ":" + PORT));
+
+console.log(process.env);
