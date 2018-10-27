b7b876d79c317a5492ae8bcfdbb23622194af3e3
Arsalan Sadri <asadri62@gmail.com>
2018-10-27 12:15:21 -0700

    minor changes

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = require("./Routes/routes-3");
 
 const app = express();
+
 app.use(router);
 
 const PORT = process.env.PORT || 8080;
 const host = "localhost";
-app.listen(PORT, host, () => console.log("App running on http://" + host + ":" + PORT));
+app.listen(PORT, host, () =>
+    console.log("App running on http://" + host + ":" + PORT));
